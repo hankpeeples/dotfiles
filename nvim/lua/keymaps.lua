ac413bf00daba9976a7052ca3aca4e8a2e92da0a
HenryPeeples <hgpeeples48@gmail.com>
2022-10-06 17:58:59 -0500

    Update autocomplete keybind

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -39,8 +39,8 @@ function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
 end
--- Use  to trigger completion
-map("i", "<S-space>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<S-space>" : coc#refresh()', { silent = true })
+-- Use to trigger completion || `<S-space>` only works correctly in .lua files, don't know why.
+map("i", "<S-space>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<S-space>" : coc#refresh()', { silent = true, expr = true })
 map("i", "<S-TAB>", [[ coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], { silent = true })
 -- Use K to show documentation in preview window.
 function _G.show_docs()
