2787259a40d74c924464d31416c1fdc429232c44
HenryPeeples <hgpeeples48@gmail.com>
2022-10-12 00:46:19 -0500

    Keymap update

--- lvim/lua/keymaps.lua
+++ lvim/lua/keymaps.lua
@@ -30,7 +30,6 @@ lvim.keys.normal_mode["<leader>cd"] = ":lua require('crates').open_documentation
 lvim.keys.normal_mode["<leader>rr"] = ":RustRun<CR>"
 
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
@@ -40,18 +39,18 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
-local _, actions = pcall(require, "telescope.actions")
-lvim.builtin.telescope.defaults.mappings = {
-    -- for input mode
-    i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-    },
-    -- for normal mode
-    n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-    },
-}
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--     -- for input mode
+--     i = {
+--         ["<C-j>"] = actions.move_selection_next,
+--         ["<C-k>"] = actions.move_selection_previous,
+--         ["<C-n>"] = actions.cycle_history_next,
+--         ["<C-p>"] = actions.cycle_history_prev,
+--     },
+--     -- for normal mode
+--     n = {
+--         ["<C-j>"] = actions.move_selection_next,
+--         ["<C-k>"] = actions.move_selection_previous,
+--     },
+-- }
