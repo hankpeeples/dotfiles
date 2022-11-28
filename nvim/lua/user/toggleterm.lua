fa3b763c01fa28a1495936415bf4a3217f260af3
Henry Peeples <hgpeeples48@gmail.com>
2022-11-28 07:36:40 -0600

    Update toggleterm keybinds

--- nvim/lua/user/toggleterm.lua
+++ nvim/lua/user/toggleterm.lua
@@ -22,7 +22,7 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
 	local opts = { noremap = true }
-	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "ii", [[<C-\><C-n>]], opts)
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
