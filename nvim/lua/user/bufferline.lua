08bf0540f1608b20e2056f92757dd749f2394b2e
Henry Peeples <hgpeeples48@gmail.com>
2022-12-04 17:34:57 -0600

    Update bufferline

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -7,8 +7,8 @@ bufferline.setup({
 	options = {
 		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
 		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-		offsets = { { filetype = "NvimTree", text = "File Explorer", padding = 1 } },
-		separator_style = "slant", -- | "thick" | "thin" | "slant" | "padded_slant"
+		offsets = { { filetype = "NvimTree", text = "File Explorer", padding = 0 } },
+		separator_style = "thick", -- | "thick" | "thin" | "slant" | "padded_slant"
 		-- indicator = {
 		-- 	style = "underline",
 		-- },
@@ -24,7 +24,7 @@ bufferline.setup({
 			items = {
 				{
 					name = "Tests", -- Mandatory
-					highlight = { underline = true, sp = "blue" }, -- Optional
+					-- highlight = { underline = true, sp = "blue" }, -- Optional
 					priority = 4, -- determines where it will appear relative to other groups (Optional)
 					icon = "", -- Optional
 					matcher = function(buf) -- Mandatory
@@ -54,7 +54,7 @@ bufferline.setup({
 				},
 				{
 					name = "Docs",
-					highlight = { underline = true, sp = "#2eda2e" },
+					-- highlight = { underline = true, sp = "#2eda2e" },
 					priority = 5,
 					auto_close = true, -- whether or not close this group if it doesn't contain the current buffer
 					matcher = function(buf)
@@ -65,6 +65,17 @@ bufferline.setup({
 		},
 	},
 	highlights = {
-		-- indicator_selected = { fg = "#ffb229", bg = "#ffb229" },
+		-- 	separator_selected = {
+		-- 		fg = "#f0907b",
+		-- 		bg = "#ffb229",
+		-- 	},
+		-- 	separator_visible = {
+		-- 		fg = "#5558da",
+		-- 		bg = "#5558da",
+		-- 	},
+		-- 	separator = {
+		-- 		fg = "#cc0000",
+		-- 		bg = "#cc0000",
+		-- 	},
 	},
 })
