8a6638a943dc6ae012a06cb79a4c456be964dab3
Henry Peeples <hgpeeples48@gmail.com>
2022-12-10 00:34:54 -0600

    Add default notify config

--- nvim/lua/user/notify.lua
+++ nvim/lua/user/notify.lua
@@ -1,1 +1,17 @@
-require("notify").setup({})
+require("notify").setup({
+	background_colour = "Normal",
+	fps = 30,
+	icons = {
+		DEBUG = "",
+		ERROR = "",
+		INFO = "",
+		TRACE = "✎",
+		WARN = "",
+	},
+	level = 2,
+	minimum_width = 50,
+	render = "minimal",
+	stages = "slide",
+	timeout = 3000,
+	top_down = true,
+})
