a3963c783599db08496ebaa4e667296ae857fc5b
Henry Peeples <hgpeeples48@gmail.com>
2023-01-15 18:19:54 -0600

    guess

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,14 +1,3 @@
--- local colorscheme = "gruvbox-material"
--- vim.g.gruvbox_material_background = "soft" -- Hard, medium, soft
--- vim.g.gruvbox_material_foreground = "original" -- Mix, material, original
--- vim.g.gruvbox_material_better_performance = 1
--- vim.g.gruvbox_material_enable_bold = 1
--- vim.g.gruvbox_material_enable_italic = 1
--- vim.g.gruvbox_material_transparent_background = 0
--- vim.g.gruvbox_material_diagnostic_text_highlight = 1
--- vim.g.gruvbox_material_diagnostic_line_highlight = 1
--- vim.cmd("set background=dark")
-
 -- local colorscheme = "one_monokai"
 -- local colors = require("one_monokai.colors")
 -- require("one_monokai").setup({
@@ -17,9 +6,9 @@
 -- 	},
 -- 	themes = function(colors)
 -- 		return {
--- 			Normal = { bg = colors.bg },
--- 			CursorLine = { bg = "#1d1f1f" },
--- 			VertSplit = { fg = "#1d1f1f" },
+-- 			-- Normal = { bg = colors.bg },
+-- 			-- CursorLine = { bg = "#1d1f1f" },
+-- 			-- VertSplit = { fg = "#1d1f1f" },
 -- 			-- Git diff
 -- 			DiffChange = { fg = colors.yellow },
 -- 			diffChanged = { fg = colors.yellow },
@@ -30,53 +19,56 @@
 -- 	end,
 -- })
 
-local colorscheme = "nightfox"
-require("nightfox").setup({
-	options = {
-		-- Compiled file's destination location
-		compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-		compile_file_suffix = "_compiled", -- Compiled file suffix
-		transparent = true, -- Disable setting background
-		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-		dim_inactive = true, -- Non focused panes set to alternative background
-		module_default = true, -- Default enable value for modules
-		styles = { -- Style to be applied to different syntax groups
-			comments = "italic", -- Value is any valid attr-list value `:help attr-list`
-			conditionals = "NONE",
-			constants = "italic",
-			functions = "bold",
-			keywords = "NONE",
-			numbers = "NONE",
-			operators = "bold",
-			strings = "NONE",
-			types = "NONE",
-			variables = "NONE",
-		},
-		inverse = { -- Inverse highlight for different types
-			match_paren = false,
-			visual = false,
-			search = false,
-		},
-		modules = { -- List of various plugins and additional options
-			-- ...
-		},
-	},
-	themes = function(colors)
-		return {
-			Normal = { bg = colors.bg },
-			-- CursorLine = { bg = "#1a2029" },
-			-- VertSplit = { fg = "#888888" },
-			-- Git diff
-			DiffChange = { fg = colors.yellow },
-			diffChanged = { fg = colors.yellow },
-			diffFile = { fg = colors.yellow },
-			-- LSP Float
-			LspInfoBorder = { bg = colors.white, fg = colors.white },
-		}
-	end,
-})
+-- local colorscheme = "onedark"
+-- require("onedark").setup({
+-- 	-- Main options --
+-- 	style = "deep", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+-- 	transparent = true, -- Show/hide background
+-- 	term_colors = true, -- Change terminal color as per the selected theme style
+-- 	ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
+-- 	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+--
+-- 	-- toggle theme style ---
+-- 	toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+-- 	toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+--
+-- 	-- Change code style ---
+-- 	-- Options are italic, bold, underline, none
+-- 	-- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+-- 	code_style = {
+-- 		comments = "italic",
+-- 		keywords = "none",
+-- 		functions = "bold",
+-- 		strings = "none",
+-- 		variables = "italic",
+-- 	},
+--
+-- 	-- Lualine options --
+-- 	lualine = {
+-- 		transparent = false, -- lualine center bar transparency
+-- 	},
+--
+-- 	-- Custom Highlights --
+-- 	colors = {}, -- Override default colors
+-- 	highlights = {}, -- Override highlight groups
+--
+-- 	-- Plugins Config --
+-- 	diagnostics = {
+-- 		darker = true, -- darker colors for diagnostic
+-- 		undercurl = true, -- use undercurl instead of underline for diagnostics
+-- 		background = true, -- use background color for virtual text
+-- 	},
+-- })
 
--- local colorscheme = "zephyr"
+local colorscheme = "sonokai"
+vim.g.sonokai_style = "andromeda"
+vim.g.sonokai_better_performance = 1
+vim.g.sonokai_enable_italic = 1
+vim.g.sonokai_cursor = "green"
+vim.g.sonokai_transparent_background = 1
+vim.g.sonokai_spell_foreground = "colored"
+vim.g.sonokai_diagnostic_virtual_text = "colored"
+vim.g.sonokai_disable_terminal_colors = 1
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
