80cc40db8168d65fe3a52d1433da460598152d18
Henry Peeples <hgpeeples48@gmail.com>
2023-01-21 13:29:53 -0600

    Update rust dev setup

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -8,6 +8,8 @@ local nor_opts = { silent = true, noremap = true }
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
+keymap("n", ";", ":", opts)
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
@@ -122,10 +124,10 @@ vim.cmd([[
   vnoremap <C-k> :m'<-2<cr>`>my`<mzgv`yo`z
 ]])
 
--- keymap("n", "<S-k>", "<cmd>lua require'rust-tools'.hover_actions.hover_actions()<cr>", opts)
--- keymap("n", "<leader>ra", "<cmd>lua require'rust-tools'.code_action_group.code_action_group()<cr>", opts)
--- keymap("n", "<leader>rr", "<cmd>lua require'rust-tools'.runnables.runnables()<cr>", opts)
--- keymap("n", "<leader>rc", "<cmd>lua require'rust-tools'.open_cargo_toml.open_cargo_toml()<cr>", opts)
--- keymap("n", "<leader>rp", "<cmd>lua require'rust-tools'.parent_module.parent_module()<cr>", opts)
--- keymap("n", "<leader>ro", "<cmd>lua require'rust-tools'.inlay_hints.enable()<cr>", opts)
--- keymap("n", "<leader>rO", "<cmd>lua require'rust-tools'.inlay_hints.disable()<cr>", opts)
+keymap("n", "<S-k>", "<cmd>lua require'rust-tools'.hover_actions.hover_actions()<cr>", opts)
+keymap("n", "<leader>ra", "<cmd>lua require'rust-tools'.code_action_group.code_action_group()<cr>", opts)
+keymap("n", "<leader>rr", "<cmd>lua require'rust-tools'.runnables.runnables()<cr>", opts)
+keymap("n", "<leader>rc", "<cmd>lua require'rust-tools'.open_cargo_toml.open_cargo_toml()<cr>", opts)
+keymap("n", "<leader>rp", "<cmd>lua require'rust-tools'.parent_module.parent_module()<cr>", opts)
+keymap("n", "<leader>ro", "<cmd>lua require'rust-tools'.inlay_hints.enable()<cr>", opts)
+keymap("n", "<leader>rO", "<cmd>lua require'rust-tools'.inlay_hints.disable()<cr>", opts)
