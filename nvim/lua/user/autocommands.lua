54b7401b39c6cc3b9601dd9c7349e97f0ff301bc
Henry Peeples <hgpeeples48@gmail.com>
2023-02-01 16:41:18 -0600

    Update auto open for nvim tree

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -24,6 +24,28 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
+local function open_nvim_tree(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- create a new, empty buffer
+	vim.cmd.enew()
+	-- wipe the directory buffer
+	vim.cmd.bw(data.buf)
+	-- change to the directory
+	vim.cmd.cd(data.file)
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter " }, {
+	callback = open_nvim_tree,
+})
+
 -- Honestly, no clue what this does
 vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_1' . tabpagenr() | quit | endif")
 
@@ -51,8 +73,8 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	pattern = { "*.ts", "*.js", "*.tsx", ".jsx", "*.json" },
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = { "*.ts", "*.js", "*.tsx", ".jsx", "*.json", "*.css" },
 	callback = function()
 		vim.cmd("Prettier")
 	end,
