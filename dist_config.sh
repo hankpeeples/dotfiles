18ce4ffb196b0b5af172c95e7dd9425cfec3b19f
Henry Peeples <hgpeeples48@gmail.com>
2023-08-06 17:06:14 -0500

    Add repo distribution script

---
+++ dist_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# exit if dotfiles is not in home dir
+if [ ! -d "$HOME/dotfiles" ]
+then
+  echo "'dotfiles' was not found in $HOME ... exiting"
+  exit 1
+fi
+
+REPO="$HOME/dotfiles"
+CONFIG_DIR="$HOME/.config"
+FONT_DIR="$HOME/.local/share"
+
+rsync -a "$REPO/kitty" "$CONFIG_DIR" && echo "Updated Kitty Config..."
+rsync -a "$REPO/nvim" "$CONFIG_DIR" && echo "Updated Neovim Config..."
+rsync -a "$REPO/btop" "$CONFIG_DIR" && echo "Updated Btop Config..."
+
+rsync "$REPO/.zshrc" "$HOME" && echo "Updated ZSH Config..."
+rsync "$REPO/starship.toml" "$CONFIG_DIR" && echo "Updated Starship Config..."
+
+rsync -r "$REPO/hypr" "rsync -a "$REPO/revive" "$CONFIG_DIR" && echo "Updated revive config..."
+
